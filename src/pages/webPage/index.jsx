f247dd033cdd34c6ad331b98e00cc49185c430c4
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-07-14 16:47:27 +0530

    About Us Page Done

--- src/pages/webPage/index.jsx
+++ src/pages/webPage/index.jsx
@@ -1,63 +1,114 @@
 import {Component} from "react";
 import {styleSheet} from "./style";
 import {withStyles} from "@mui/styles";
-import {Button, Grid} from "@mui/material";
+import {Button, Grid, Typography} from "@mui/material";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-class WebPage extends Component{
+class WebPage extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        const {classes}=this.props
-        return(
-        <Grid className={classes.container}>
-            <Grid className={classes.home_container}>
-                <Grid className={classes.navBar}>
-                    <Tabs
-                        centered
-                        textColor="secondary"
-                        indicatorColor="secondary"
-                        aria-label="secondary tabs example"
-                    >
-                        <Tab value="one" label="Home"/>
-                        <Tab value="two" label="Ride"/>
-                        <Tab value="three" label="Services"/>
-                        <Tab value="four" label="About"/>
-                        <Tab value="four" label="Reviews"/>
-                        <Button variant="contained" style={{marginLeft: '700px', marginTop: '12px', height: '32px'}}
-                        >Sing In</Button>
-                        <Button variant="contained" style={{marginLeft: '20px', marginTop: '12px', height: '32px'}}>Sing
-                            Up</Button>
+        const {classes} = this.props
+        return (
+            <Grid className={classes.container}>
+                <Grid className={classes.home_container}>
+                    <Grid className={classes.navBar}>
+                        <Tabs
+                            centered
+                            textColor="secondary"
+                            indicatorColor="secondary"
+                            aria-label="secondary tabs example"
+                        >
+                            <Tab value="one" label="Home" className={classes.navItems}/>
+                            <Tab value="two" label="Ride" className={classes.navItems}/>
+                            <Tab value="three" label="Services" className={classes.navItems}/>
+                            <Tab value="four" label="About" className={classes.navItems}/>
+                            <Tab value="four" label="Reviews" className={classes.navItems}/>
+                            <Button variant="outlined" endIcon={<AccountCircleIcon/>} style={{borderColor: 'red',color:'red'}}>
+                                SignIn
+                            </Button>
+                        </Tabs>
+                    </Grid>
+                    <Grid className={classes.home_container_right}>
+                        <Grid className={classes.home_left}>
+                            <Typography variant="h3" component="div" gutterBottom
+                                        className={classes.home_container_right_p}
+                                        style={{fontWeight: '600', marginLeft: '65px', marginTop: '150px',color:'#ff4529'}}>
+                                Search, book,and rent vehicle easily
+                            </Typography>
+                            <Typography variant="body2" gutterBottom style={{marginBottom: '10px',color:'#b35a2b'}}>
+                                Rent an car for your travel needs wherever and whenever you want with your device!
+                            </Typography>
+                        </Grid>
+                        <Grid className={classes.home_left}>
+                            <Button variant="outlined" size="large"
+                                    style={{marginBottom: '200px', marginRight: '370px',borderColor: 'red',color:'red'}}>
+                                Reserve Now
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
 
-                    </Tabs>
+                <Grid className={classes.about_container}>
+                    <Grid className={classes.about_container_left}>
+
+                    </Grid>
+                    <Grid className={classes.about_container_right}>
+                        <Grid className={classes.about_right}>
+                            <Grid className={classes.about_right_1}>
+                                <Typography variant="h5" gutterBottom component="div" style={{color:'red'}}>
+                                    About Us
+                                </Typography>
+                            </Grid>
+                            <Grid className={classes.about_right_2}>
+                                <Typography variant="h4" gutterBottom component="div" style={{fontWeight:'600',color:'#ff4529'}}>
+                                    Welcome to Car Rent Service
+                                </Typography>
+                                <Typography variant="body1" gutterBottom style={{paddingLeft:'27px',paddingTop:'20px',color:'#b35a2b'}}>
+                                    body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                                    blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+                                    neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+                                    quasi quidem quibusdam.
+                                </Typography>
+                            </Grid>
+
+                            <Grid className={classes.about_right_3}>
+                                <Grid>
+                                    <Typography variant="overline" display="block" gutterBottom style={{paddingBottom:'20px',color:'#ff0000'}}>
+                                        24/7 Service
+                                    </Typography>
+                                    <Typography variant="overline" display="block" gutterBottom style={{paddingBottom:'20px',color:'#ff0000'}}>
+                                       Island Wide Service
+                                    </Typography>
+                                    <Typography variant="overline" display="block" gutterBottom style={{paddingBottom:'20px',color:'#ff0000'}}>
+                                        Easy Payment
+                                    </Typography>
+                                </Grid>
+                                <Grid>
+                                    <Typography variant="overline" display="block" gutterBottom style={{paddingBottom:'20px',color:'#ff0000'}}>
+                                        Luxury Cars
+                                    </Typography>
+                                    <Typography variant="overline" display="block" gutterBottom style={{paddingBottom:'20px',color:'#ff0000'}}>
+                                        Price Negotiable
+                                    </Typography>
+                                    <Typography variant="overline" display="block" gutterBottom style={{paddingBottom:'20px',color:'#ff0000'}}>
+                                        Card Accepted
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Grid className={classes.vehicle_container}>
+
                 </Grid>
             </Grid>
-
-            <Grid className={classes.home_container}>
-                <Grid className={classes.navBar}>
-                    <Tabs
-                        centered
-                        textColor="#2196f3"
-                        indicatorColor="#2196f3"
-                        aria-label="secondary tabs example"
-                    >
-                        <Tab value="one" label="Home"/>
-                        <Tab value="two" label="Ride"/>
-                        <Tab value="three" label="Services"/>
-                        <Tab value="four" label="About"/>
-                        <Tab value="four" label="Reviews"/>
-                        <Button variant="contained" style={{marginLeft: '700px', marginTop: '12px', height: '32px'}}
-                        >Sing In</Button>
-                        <Button variant="contained" style={{marginLeft: '20px', marginTop: '12px', height: '32px'}}>Sing
-                            Up</Button>
-
-                    </Tabs>
-                </Grid>
-            </Grid>
-        </Grid>
         )
     }
 }
--- src/pages/webPage/style.js
+++ src/pages/webPage/style.js
@@ -1,4 +1,6 @@
-import main from "../../assets/images/main.jpg";
+import main1 from "../../assets/images/main1.png";
+import about1 from "../../assets/images/about1.png";
+import homeimg from "../../assets/images/homeimg.png";
 
 export const styleSheet={
     container:{
@@ -9,7 +11,7 @@ export const styleSheet={
     home_container:{
         width:'100vw',
         height:'100vh',
-        backgroundImage: `url(${main})`,
+        backgroundImage: `url(${main1})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         display: 'flex',
@@ -18,7 +20,143 @@ export const styleSheet={
     },
 
     navBar:{
+        width: '100vw',
+        height: '10vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'right',
+        marginRight:'20px'
+    },
+
+    navItems:{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        fontWeight: 'bold',
+        alignItems: 'center',
+        justifyContent: 'right',
+        color:'white'
+    },
+
+    home_container_right:{
+        width: '50vw',
+        height: '90vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        // backgroundColor:'red',
+        justifyContent: 'center',
+        flexDirection:'column'
+    },
+
+    home_container_right_p:{
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        alignItems: 'center',
+        color:'black'
+    },
+
+    home_left:{
+        width: '50vw',
+        height: '45vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'center'
 
     },
+
+    about_container:{
+        width:'100vw',
+        height:'100vh',
+        // backgroundImage: `url(${main1})`,
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+    },
+
+    about_container_left:{
+        width:'55vw',
+        height:'100vh',
+        // backgroundColor:'green',
+        display: 'flex',
+        margin:'auto',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'center',
+        backgroundImage: `url(${homeimg})`,
+        backgroundRepeat: 'no-repeat',
+    },
+
+
+    about_container_right:{
+        width:'45vw',
+        height:'100vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'center',
+
+    },
+
+    about_right:{
+        width: '45vw',
+        height: '100vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'center',
+
+    },
+
+    about_right_1:{
+        width: '45vw',
+        height: '25vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'center',
+        // border:'1px solid red',
+        paddingTop:'110px',
+        paddingRight:'460px'
+    },
+
+    about_right_2:{
+        width: '45vw',
+        height: '30vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'center',
+        // border:'1px solid red',
+        paddingBottom:'100px',
+        paddingRight:'90px'
+    },
+
+    about_right_3:{
+        width: '45vw',
+        height: '45vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent:'space-evenly',
+        // border:'1px solid red',
+        flexDirection:'column',
+        paddingRight:'100px'
+
+    },
+
+    vehicle_container:{
+        width:'100vw',
+        height:'100vh',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+    },
+
 
 }
